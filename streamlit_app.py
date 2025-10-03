f6ce9aa2f3fbba8737cde039e66d9b2feefa951c
turnerville14 <jaiyaguna@live.com.sg>
2025-10-03 13:56:06 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,19 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+hide_streamlit_style = """
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Optionally, hide the footer */
+                .streamlit-footer {display: none;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-uf99v8 {display: none;}
+            </style>
+            """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # --- Session State ---
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
